b282008b79ee1fa3dee6a8846e099a31d8754e8a
Denis <thedenisnikulin@gmail.com>
2021-09-01 17:04:54 +0300

    trying out socks5 connection logic

--- rustychains/src/lib.rs
+++ rustychains/src/lib.rs
@@ -1,8 +1,9 @@
-use libc::{c_int, dlsym, sockaddr, socklen_t, RTLD_NEXT};
-use std::{ffi::{CString}, net::SocketAddr};
-use socks;
+use libc::{c_int, dlsym, sockaddr, sockaddr_in, socklen_t, RTLD_NEXT};
+use socks::Socks5Stream;
+use std::os::unix::prelude::IntoRawFd;
+use std::{ffi::CString};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
 
-// 139.99.75.11:8080
 
 type OriginalConnect =
     unsafe extern "C" fn(socket: c_int, address: *const sockaddr, address_len: socklen_t) -> c_int;
@@ -22,12 +23,30 @@ unsafe fn init_lib() -> Original {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn connect(socket: c_int, address: *const sockaddr, address_len: socklen_t) -> c_int {
+pub unsafe extern "C" fn connect(
+    socket: c_int,
+    address: *const sockaddr,
+    address_len: socklen_t,
+) -> c_int {
     let original = init_lib();
+
     let hello = CString::new("Lol, it's Rust.\n").unwrap();
     libc::printf(hello.as_ptr());
-//    let socks5_stream = socks::Socks5Stream::connect(SocketAddr::from([139, 99, 75, 11], 8080), SocketAddr::)
-    (original.connect)(socket, address, address_len)
+
+    let target = from_sockaddr(address);
+    let socks5_stream =
+        Socks5Stream::connect("139.99.75.11:8080", (*target.ip(), target.port())).unwrap();
+    // ^ test proxy addr
+    socks5_stream.into_inner().into_raw_fd() as c_int
+    // (original.connect)(socket, address, address_len)
+}
+
+unsafe fn from_sockaddr(addr: *const sockaddr) -> SocketAddrV4 {
+    // TODO directly cast s_addr to Ipv4Addr?
+    let addr_in: sockaddr_in = std::mem::transmute(*addr);
+    let ipv4_addr = Ipv4Addr::from(addr_in.sin_addr.s_addr);
+    let port = addr_in.sin_port;
+    SocketAddrV4::new(ipv4_addr, port)
 }
 
 #[cfg(test)]
